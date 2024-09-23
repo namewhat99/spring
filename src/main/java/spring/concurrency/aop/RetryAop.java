30a379d1ed944e079f5bb7b5916cede55d338664
이광훈 <leegh963@naver.com>
2024-09-23 19:54:09 +0900

    Fix : 쿠폰 번호 중복 시 재시도 AOP 작성

--- src/main/java/spring/concurrency/aop/RetryAop.java
+++ src/main/java/spring/concurrency/aop/RetryAop.java
@@ -1,4 +1,31 @@
 package spring.concurrency.aop;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.util.UUID;
+
+@Slf4j
+@Aspect
 public class RetryAop {
+
+    @Around("@annotation(spring.concurrency.annotation.Retry)")
+    public Object doRetry(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        int retry = 3;
+        Exception exception = null;
+
+        for (int i = 1; i <= retry; i++) {
+            try{
+                return joinPoint.proceed();
+            }catch (IllegalStateException e){
+                exception = e;
+            }
+        }
+
+        return exception;
+    }
 }
