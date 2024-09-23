e9c2352458f2274d847aaae6763e69088980bf77
이광훈 <leegh963@naver.com>
2024-09-23 19:44:11 +0900

    Fix : Coupon 엔티티 빌더 어노테이션 추가

--- src/main/java/spring/concurrency/entity/Coupon.java
+++ src/main/java/spring/concurrency/entity/Coupon.java
@@ -4,11 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Coupon {
 
     @Id
