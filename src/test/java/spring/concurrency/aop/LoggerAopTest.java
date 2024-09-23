aa341776ec9f3f18d3244c266f5a04021ede9bd2
이광훈 <leegh963@naver.com>
2024-09-23 19:13:14 +0900

    Test : AOP logger 테스트코드 작성

--- src/test/java/spring/concurrency/aop/LoggerAopTest.java
+++ src/test/java/spring/concurrency/aop/LoggerAopTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package spring.concurrency.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import spring.concurrency.coupon.CouponService;
+
+@Slf4j
+@Import(LoggerAop.class)
+@SpringBootTest
 class LoggerAopTest {
-  
+
+    @Autowired
+    private CouponService couponService;
+
+    @Test
+    public void loggingTest(){
+        this.couponService.publishCoupon();
+    }
+
 }
