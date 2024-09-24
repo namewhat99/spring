8867e3a293fc3bf5b27fb05d70d3f28043f3b595
이광훈 <leegh963@naver.com>
2024-09-24 19:14:09 +0900

    Fix : pk generate strategy 변경

--- src/main/java/spring/concurrency/entity/Coupon.java
+++ src/main/java/spring/concurrency/entity/Coupon.java
@@ -1,9 +1,6 @@
 package spring.concurrency.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class Coupon {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String code;
